a17bdc0fcad53efeff131423841de605eb9f77dc
Raquel Sánchez <raquelus8@gmail.com>
2026-03-21 12:36:49 +0100

    Add tests for Rover's turnLeft and turnRight methods with all directions

--- src/test/java/com/kata/model/RoverTest.java
+++ src/test/java/com/kata/model/RoverTest.java
@@ -8,20 +8,38 @@ public class RoverTest {
     public void rover_turns_left() {
         Position initialPosition = new Position(1, 2);
         Rover rover = new Rover(initialPosition, Direction.N);
+        Rover rover2 = new Rover(initialPosition, Direction.S);
+        Rover rover3 = new Rover(initialPosition, Direction.W);
+        Rover rover4 = new Rover(initialPosition, Direction.E);
 
         rover.turnLeft();
+        rover2.turnLeft();
+        rover3.turnLeft();
+        rover4.turnLeft();
 
         assertEquals(Direction.W, rover.getDirection());
+        assertEquals(Direction.E, rover2.getDirection());
+        assertEquals(Direction.S, rover3.getDirection());
+        assertEquals(Direction.N, rover4.getDirection());
     }
 
     @Test
     public void rover_turns_right() {
         Position initialPosition = new Position(1, 2);
         Rover rover = new Rover(initialPosition, Direction.N);
+        Rover rover2 = new Rover(initialPosition, Direction.S);
+        Rover rover3 = new Rover(initialPosition, Direction.W);
+        Rover rover4 = new Rover(initialPosition, Direction.E);
 
         rover.turnRight();
+        rover2.turnRight();
+        rover3.turnRight();
+        rover4.turnRight();
 
         assertEquals(Direction.E, rover.getDirection());
+        assertEquals(Direction.W, rover2.getDirection());
+        assertEquals(Direction.N, rover3.getDirection());
+        assertEquals(Direction.S, rover4.getDirection());
     }
 
     @Test
